544ffbecc626a4197d0f464b32c8f0822e38fe13
RAJAN PRATAP SINGH <53988654+LONEWOLF-tech@users.noreply.github.com>
2022-01-29 17:52:52 +0530

    Searching List Item

--- lib/MainScreenFolder/AppBar/LocationPointsSearch.dart
+++ lib/MainScreenFolder/AppBar/LocationPointsSearch.dart
@@ -130,7 +130,12 @@ class _PointsState extends State<Points> {
   }
 }
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
   final _searchController = TextEditingController();
 
   searchQuery(String query, List items) async {
@@ -148,45 +153,246 @@ class Search extends StatelessWidget {
     var h1 = MediaQuery.of(context).size.height;
     // var w1 = MediaQuery.of(context).size.width;
     return Consumer<RestaurantListProvider>(builder: (_, value, __) {
-      return Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 15,
-        ),
-        child: SizedBox(
-          height: h1 * 0.05,
-          child: TextFormField(
-            controller: _searchController,
-            onChanged: (v) async {
-              await searchQuery(_searchController.text, value.items);
-            },
-            textAlign: TextAlign.start,
-            obscureText: false,
-            decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(0),
-                hintText: 'Search Cuisines',
-                hintStyle: TextStyle(color: Colors.grey),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Color(0xFFFAB84C),
-                ),
-                focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFFFAB84C), width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(11))),
-                enabledBorder: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(11),
-                  ),
-                  borderSide: BorderSide(
-                    color: Color(0xFFFAB84C),
-                    width: 1,
-                  ),
-                )),
+      return Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15,
+            ),
+            child: SizedBox(
+              height: h1 * 0.05,
+              child: TextFormField(
+                controller: _searchController,
+                onChanged: (v) async {
+                  await searchQuery(_searchController.text, value.items);
+                },
+                textAlign: TextAlign.start,
+                obscureText: false,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(0),
+                    hintText: 'Search Cuisines',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Color(0xFFFAB84C),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xFFFAB84C), width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(11))),
+                    enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(11),
+                      ),
+                      borderSide: BorderSide(
+                        color: Color(0xFFFAB84C),
+                        width: 1,
+                      ),
+                    )),
+              ),
+            ),
           ),
-        ),
+          _searchController.text.isNotEmpty ? SearchItemList() : Container(),
+        ],
       );
     });
   }
 }
+
+class SearchItemList extends StatefulWidget {
+  const SearchItemList({Key? key}) : super(key: key);
+
+  @override
+  _SearchItemListState createState() => _SearchItemListState();
+}
+
+class _SearchItemListState extends State<SearchItemList> {
+  String isSelected = 'DELIVERY';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 15, right: 15),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.width * 0.08,
+            ),
+            height: MediaQuery.of(context).size.height * 0.08,
+            // color: Colors.grey.shade300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  // blurRadius: 6.0,
+                ),
+              ],
+              color: Colors.transparent,
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isSelected = 'DELIVERY';
+                      print(isSelected);
+                    });
+                  },
+                  child: Container(
+                    // height: MediaQuery.of(context).size.height * 0.005,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    alignment: Alignment.center,
+                    // margin: EdgeInsets.only(left: 3, right: 3),
+                    // padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          // blurRadius: 6.0,
+                        ),
+                      ],
+                      color: Colors.black,
+                    ),
+                    child: Text(
+                      'DELIVERY',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isSelected = 'DINING';
+                      print(isSelected);
+                    });
+                  },
+                  child: Container(
+                    // height: MediaQuery.of(context).size.height * 0.005,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    alignment: Alignment.center,
+                    // margin: EdgeInsets.only(left: 3, right: 3),
+                    // padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          // blurRadius: 6.0,
+                        ),
+                      ],
+                      color: Colors.transparent,
+                    ),
+                    child: Text(
+                      'DINING',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade700,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              height: 400,
+              margin: EdgeInsets.only(top: 10),
+              color: Colors.white,
+              child: ListView.builder(
+                  itemCount: 15,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SearchListItemtile(
+                      index: index,
+                    );
+                  })),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchListItemtile extends StatelessWidget {
+  final index;
+
+  const SearchListItemtile({
+    Key? key,
+    this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(5),
+        minVerticalPadding: 5,
+        tileColor: Colors.white,
+        leading: Container(
+            width: 80, child: Image.asset('assets/images/icecream.png')),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'RC IceCream',
+              style: TextStyle(
+                fontSize: 16,
+                // color: Colors.grey.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Icecream',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey.shade700,
+                // fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade300,
+                // blurRadius: 6.0,
+              ),
+            ],
+          ),
+          // color: Colors.grey.shade300,
+          child: Text(
+            'Promoted',
+            style: TextStyle(
+              fontSize: 10,
+              color: Colors.grey.shade700,
+              // fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
 
 // Container(
 //         decoration: BoxDecoration(
