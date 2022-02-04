0ee9c7705982af294ff1c9714a16e65382722e57
RAJAN PRATAP SINGH <53988654+LONEWOLF-tech@users.noreply.github.com>
2022-02-04 14:50:22 +0530

    Cart Screen UI Update

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // Colors
+  static const kGreen = Color.fromRGBO(59, 183, 85, 1);
+  static const kRed = Color.fromRGBO(226, 55, 68, 1);
+  static const kOrange = Color(0xFFFAB84C);
+  static const kYellow = Color.fromRGBO(247, 193, 43, 1);
+  static const kBlue = Colors.blue;
+  // static const kGrey =
+}
